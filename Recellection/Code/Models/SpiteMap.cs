7c5d3f1f9fe711c4bf905238a74c6c51b9cb8235
john <johnfor@kth.se>
2010-03-31 14:02:08 +0200

    Made the SpriteMap class and to not get compile error i added the Texture class as well.

---
+++ Recellection/Code/Models/SpiteMap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Recellection.Code.Models
+{
+    class SpiteMap
+    {
+        Texture2D[] textures;
+
+        public Texture2D GetTexture(Globals.Texture texture, int curFrame, 
+            int size)
+        {
+            return new Texture(textures[(int)texture], 
+                new Rectangle(curFrame * size, 0, size, size));
+        }
+
+    }
+}
