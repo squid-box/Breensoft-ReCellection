1047e4dab9ddb074a82d341fb45dd77926ca9e2f
john <johnfor@kth.se>
2010-03-31 22:24:19 +0200

    Added the missing classes hope it compiles now.

---
+++ Recellection/Code/Models/BaseBuilding.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Models.Building;
+
+namespace Recellection.Code.Models
+{
+    class BaseBuilding : Building
+    {
+
+        public void Visit(Building building)
+        {
+            //TODO Add functionality here.
+        }
+
+    }
+}
