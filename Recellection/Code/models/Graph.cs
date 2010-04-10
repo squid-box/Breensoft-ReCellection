d8b3d7cbd937e2f052ec1ce73915fb18301d3d09
martin <martin.nycander@gmail.com>
2010-04-10 19:06:33 +0200

    Documented and tested Graph

--- Recellection/Code/models/Graph.cs
+++ Recellection/Code/models/Graph.cs
@@ -6,29 +6,59 @@ using System.Text;
 namespace Recellection.Code.Models
 {
 	/**
-	* The Graph component is a storage class for buildings and their weights.
-	*/
+	 * The Graph component is a storage class for buildings and their weights.
+	 * 
+	 * TODO: Implement Publisher-pattern.
+	 * 
+	 * @author Martin Nycander
+	 */
 	public class Graph
 	{
 		private static int defaultWeight = 1;
 
 		private Dictionary<Building, int> buildings;
 
+		/**
+		 * Constructs and initializes an empty graph.
+		 */
 		public Graph()
 		{
 			buildings = new Dictionary<Building, int>();
 		}
 
+		/**
+		 * Adds a building to the graph.
+		 * A building that has already been added will be ignored.
+		 * 
+		 * @param building the building to add
+		 */
 		public void Add(Building building)
 		{
+			if (buildings.ContainsKey(building))
+			{
+				return;
+			}
+			
 			buildings.Add(building, defaultWeight);
 		}
 
+		/**
+		 * Removes a building from the graph.
+		 * 
+		 * @param building the building to remove
+		 */
 		public void Remove(Building building)
 		{
 			buildings.Remove(building);
 		}
 
+		/**
+		 * Sets the weight of a building node in the graph.
+		 * The building is added to the graph, if it is not a part of the graph.
+		 * 
+		 * @param building the building to set weight for.
+		 * @param weight the new weight.
+		 */
 		public void SetWeight(Building building, int weight)
 		{
 			if (! buildings.ContainsKey(building))
@@ -39,19 +69,25 @@ namespace Recellection.Code.Models
 			buildings[building] = weight;
 		}
 
+		/**
+		 * @return the weight of the building.
+		 * @throws ArgumentException if the building is not a part of the graph.
+		 */
 		public int GetWeight(Building building)
 		{
 			int weight;
 			
 			if (! buildings.TryGetValue(building, out weight))
 			{
-				//throw new ArgumentException("That building does not exist.");
-				weight = 0;
+				throw new ArgumentException("That building does not exist in this graph.");
 			}
 			
 			return weight;
 		}
 		
+		/**
+		 * @return the number of buildings in this graph.
+		 */
 		public int CountBuildings()
 		{
 			return buildings.Count();
--- RecellectionTests/Code/Models/GraphTest.cs
+++ RecellectionTests/Code/Models/GraphTest.cs
@@ -7,11 +7,18 @@ using NUnit.Framework;
 
 namespace Recellection.Code.Models
 {
+	class TestBuilding : Building
+	{
+	}
+	
 	[TestFixture]
 	public class GraphTest
 	{
 		private Graph g;
 		
+		private Building b1 = new TestBuilding();
+		private Building b2 = new TestBuilding();
+		private Building b3 = new TestBuilding();
 		
 		[SetUp]
 		public void Init()
@@ -20,9 +27,62 @@ namespace Recellection.Code.Models
 		}
 	
 		[Test]
-		public void Add()
+		public void AddAndCount()
 		{
+			g.Add(b1);
+			Assert.AreEqual(1, g.CountBuildings());
+			g.Add(b2);
+			Assert.AreEqual(2, g.CountBuildings());
+			g.Add(b3);
+			Assert.AreEqual(3, g.CountBuildings());
 			
+			// Adding same building twice should be ignored
+			g.Add(b1);
+			Assert.AreEqual(3, g.CountBuildings());
+		}
+		
+		[Test]
+		public void Remove()
+		{
+			g.Add(b1);
+			Assert.AreEqual(1, g.CountBuildings());
+
+			g.Remove(b1);
+			Assert.AreEqual(0, g.CountBuildings());
+
+			g.Remove(b1);
+			Assert.AreEqual(0, g.CountBuildings());
+		}
+		
+		[Test]
+		public void SetWeight()
+		{
+			g.Add(b1);
+			g.SetWeight(b1, 100);
+			g.SetWeight(b2, 200);
+			
+			Assert.AreEqual(2, g.CountBuildings());
+		}
+		
+		[Test]
+		public void GetWeight()
+		{
+			g.Add(b1);
+			g.SetWeight(b1, 100);
+			Assert.AreEqual(100, g.GetWeight(b1));
+			g.SetWeight(b1, 200);
+			Assert.AreEqual(200, g.GetWeight(b1));
+		}
+		
+		[Test]
+		public void GetWeightOfNonExistingBuilding()
+		{
+			Assert.Throws<ArgumentException>(delegate{ g.GetWeight(b1); });
+			
+			g.Add(b1);
+			g.Remove(b1);
+			
+			Assert.Throws<ArgumentException>(delegate { g.GetWeight(b1); });
 		}
 	}
 }
