48841a0ce15897bbacbf574548949b845da8cb10
martin <martin.nycander@gmail.com>
2010-04-10 15:55:01 +0200

    Added Logger-class

---
+++ Recellection/Code/Main/Logger.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace Recellection.Code.Main
+{
+	public class Logger
+	{
+		private enum LogLevel { TRACE = 1, DEBUG = 2, INFO = 3, WARN = 4, ERROR = 5, FATAL = 6 };
+		
+		private static LinkedList<Logger> loggers = new LinkedList<Logger>();
+		private static LogLevel globalThreshold = LogLevel.TRACE;
+
+		public static Logger getLogger(string name)
+		{
+			Logger l = new Logger(name);
+
+			loggers.AddLast(l);
+
+			return l;
+		}
+		
+		public static Logger getLogger()
+		{
+			StackTrace stackTrace = new StackTrace();
+			StackFrame stackFrame = stackTrace.GetFrame(1);
+			string className = stackFrame.GetMethod().ReflectedType.FullName;
+			return getLogger(className);
+		}
+		
+		private string name;
+		private LogLevel threshold;
+
+		private Logger(string name)
+		{
+			this.name = name;
+			this.threshold = LogLevel.TRACE;
+		}
+		
+		public string GetName()
+		{
+			return name;
+		}
+		
+		private void Log(string message, LogLevel level)
+		{
+			if (level < this.threshold)
+				return;
+			
+			if (level < Logger.globalThreshold)
+				return;
+			
+			System.Console.WriteLine(message);
+		}
+		
+		public void Trace(string message)
+		{
+			Log(message, LogLevel.TRACE);
+		}
+		
+		public void Debug(string message)
+		{
+			Log(message, LogLevel.DEBUG);
+		}
+		
+		public void Info(string message)
+		{
+			Log(message, LogLevel.INFO);
+		}
+		
+		public void Warn(string message)
+		{
+			Log(message, LogLevel.WARN);
+		}
+		
+		public void Error(string message)
+		{
+			Log(message, LogLevel.ERROR);
+		}
+		
+		public void Fatal(string message)
+		{
+			Log(message, LogLevel.FATAL);
+		}
+	}
+}
---
+++ RecellectionTests/LoggerTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+using Recellection.Code.Main;
+
+namespace RecellectionTests
+{
+	[TestFixture]
+	class LoggerTest
+	{
+		private Logger l;
+		
+		[SetUp]
+		public void init()
+		{
+		}
+		
+		[Test]
+		public void GetLoggerName()
+		{
+			l = Logger.getLogger("Test");
+			Assert.AreEqual("Test", l.GetName());
+		}
+		
+		[Test]
+		public void GetLoggerAuto()
+		{
+			l = Logger.getLogger();
+			Assert.AreEqual("RecellectionTests.LoggerTest", l.GetName());
+		}
+	}
+}
