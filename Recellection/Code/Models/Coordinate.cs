ca235233dc25eace30793c7c87c31aad9726011b
Glader <lukasm@kth.se>
2010-04-11 18:26:59 +0200

    hoppas att git ska gilla mina filer nu.

---
+++ Recellection/Code/Models/Coordinate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Recellection.Code.Models
+{
+    class Coordinate
+    {
+        public int x;
+        public int y;
+
+    }
+}
