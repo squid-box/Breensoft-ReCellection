8b58b6a4e835e400b76b795538c5cbda83cfcd08
squid-box <joelah@kth.se>
2010-04-11 14:40:02 +0200

    This won't end well...
    Signed-off-by: squid-box <joelah@kth.se>

--- Recellection/Code/Models/TerrainType.cs
+++ Recellection/Code/Models/TerrainType.cs
@@ -3,28 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Recellection.Code.Models
+namespace Recellection
 {
+    /* Contains information about traits of different Terrain Types.
+     * 
+     * Author: Joel Ahlgren
+     * Date: 2010-04-11
+     */
     class TerrainType
     {
-        /**
+        /*
          * Data
          */
-        private int dmgMod;
-        private int spdMod;
-        private int rscMod;
+        private int dmgMod; // Damage modifier
+        private int spdMod; // Speed modifier
+        private int rscMod; // Resource modifier
 
-        private enum type
-        {
-            Mucus, Membrane, Water, Slow, Poisoned
-        }
-
-        /**
+        /*
          * Methods
          */
         public TerrainType()
         {
-            
+            // Assume default type.
+            this.dmgMod = 0;
+            this.spdMod = 10;
+            this.rscMod = 10;
+        }
+        public TerrainType(Globals.TerrainTypes t)
+        {
+            switch (t)
+            {
+                case(Globals.TerrainTypes.Membrane):
+                {
+                    this.dmgMod = 0;
+                    this.spdMod = 10;
+                    this.rscMod = 10;
+                    break;
+                }
+                case (Globals.TerrainTypes.Mucus):
+                {
+                    this.dmgMod = 0;
+                    this.spdMod = 8;
+                    this.rscMod = 12;
+                    break;
+                }
+                case (Globals.TerrainTypes.Slow):
+                {
+                    this.dmgMod = 0;
+                    this.spdMod = 5;
+                    this.rscMod = 10;
+                    break;
+                }
+                case (Globals.TerrainTypes.Infected):
+                {
+                    this.dmgMod = 5;
+                    this.spdMod = 10;
+                    this.rscMod = 5;
+                    break;
+                }
+            }
         }
     }
 }
