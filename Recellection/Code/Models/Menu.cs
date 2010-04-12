35c56469e754e1802598ecc636fc32ebca5a0aab
co <coer@kth.se>
2010-04-12 14:49:33 +0200

    Prototyp till menu model

--- Recellection/Code/Models/Menu.cs
+++ Recellection/Code/Models/Menu.cs
@@ -3,9 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+/**
+ * 
+ * Author: co
+ * 
+ **/
+
 namespace Recellection.Code.Models
 {
     class Menu
     {
+        private List<Region> regions;
+        private Texture menuPic;
+        private Menu helpMenu; //om denna Menu är en helpmenu eller inte ska ha en helpMenu sets denna variabel till null duh.
+
+
+        /**
+         * 
+         * min tanke är att varje meny ska vara ett Menu objekt. 
+         * Denna klass kommer ha två konstruktorer den beskriver exakt hur menyn ska se ut och i den andra kan man välja bland några hårdkodade menyer.
+         * Om du har synpunkter eller frågorom implementationen tveka inte att kontakta mig (co).
+         * 
+         * Jag tänker inte låta det vara möjligt att ändra menyer, man gör bara nya om så skulle behövas istället, om dett visar sig vara ineffektivt ändrar jag på det.
+         * 
+         * */
+
+
+        //place holders dem riktiga funktionerna ska faktiskt göra något :)
+        public Menu()
+        {
+            return null;
+        }
+
+        //här kommer menyerna hårdkodas, mest kod här
+        public Menu(String menuType)
+        {
+            return null;
+        }
+
+
+        //och så några get metoder:
+        public List<Region> GetRegions()
+        {
+            return regions;
+        }
+        public Texture GetMenuPic()
+        {
+            return menuPic;
+        }
+        public Menu GetHelp() //seriously dude, you need help...
+        {
+            return helpMenu;
+        }+
+        
     }
 }
