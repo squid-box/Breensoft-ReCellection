ee21fdc1d538e2cbbd9f31da4b195e4028c71965
squid-box <joelah@kth.se>
2010-04-12 22:34:08 +0200

    Tests written for Tile.cs and Unit.cs. Fixed some References in RecellectionTests.csproj.
    Signed-off-by: squid-box <joelah@kth.se>

--- RecellectionTests/Code/Models/TileTest.cs
+++ RecellectionTests/Code/Models/TileTest.cs
@@ -8,20 +8,99 @@ namespace Recellection.Code.Models
 {
     [TestFixture]
     class TileTest
-    {
+    {  
         Tile t1, t2;
+        Player p;
+        Unit u1, u2, u3;
+        Building b1, b2;
+
         [SetUp]
         public void init()
         {
             t1 = new Tile();
             t2 = new Tile(Globals.TerrainTypes.Slow);
+            p = new Player();
+            u1 = new Unit();
+            u2 = new Unit();
+            u3 = new Unit();
+            //b1 = new Building();
         }
 
         [Test]
-        public void createTest()
+        public void CheckType()
         {
-            Assert.IsNotNull(t1);
-            Assert.IsNotNull(t2);
+            Assert.AreEqual(Globals.TerrainTypes.Membrane, t1.GetTerrainType().GetEnum());
+            Assert.AreEqual(Globals.TerrainTypes.Slow, t2.GetTerrainType().GetEnum());
+
+            t1.ChangeTerrainType(Globals.TerrainTypes.Mucus);
+
+            Assert.AreEqual(Globals.TerrainTypes.Mucus, t1.GetTerrainType().GetEnum());
+        }
+
+        [Test]
+        public void VisibleTo()
+        {
+            Assert.IsFalse(t1.IsVisible(p));
+            t1.MakeVisibleTo(p);
+            Assert.IsTrue(t1.IsVisible(p));
+            Assert.IsFalse(t2.IsVisible(p));
+
+            t1.MakeInvisibleTo(p);
+            Assert.IsFalse(t1.IsVisible(p));
+        }
+
+        [Test]
+        public void EqualsTest()
+        {
+            Assert.IsFalse(t1.Equals(t2));
+            t1.ChangeTerrainType(t2.GetTerrainType().GetEnum());
+
+            Assert.IsTrue(t1.Equals(t2));
+        }
+
+        [Test]
+        public void UnitTest()
+        {
+            Assert.AreEqual(0, t1.GetUnits().Count);
+            t1.AddUnit(u1);
+            List<Unit> l1 = new List<Unit>();
+            l1.Add(u2);
+            l1.Add(u3);
+            t1.AddUnit(l1);
+
+            Assert.AreEqual(3, t1.GetUnits().Count);
+
+            t1.AddUnit(u2);
+            Assert.AreEqual(3, t1.GetUnits().Count);
+
+            t1.RemoveUnit(u1);
+            Assert.AreEqual(2, t1.GetUnits().Count);
+
+            t1.RemoveUnit(u1);
+            Assert.AreEqual(2, t1.GetUnits().Count);
+
+            t1.RemoveUnit(l1);
+            Assert.AreEqual(0, t1.GetUnits().Count);
+        }
+
+        [Test]
+        public void OverrideOperator()
+        {
+            t1.ChangeTerrainType(t2.GetTerrainType().GetEnum());
+            Assert.IsTrue(t1.Equals(t2));
+            Assert.IsTrue(t1 == t2);
+        }
+
+        [Test]
+        public void BuildingTest()
+        {
+            Assert.IsNull(t1.GetBuilding());
+            Assert.IsTrue(t1.SetBuilding(b1));
+            Assert.IsFalse(t1.SetBuilding(b2));
+
+            t1.RemoveBuilding();
+
+            Assert.IsNull(t1.GetBuilding());
         }
     }
 }
--- RecellectionTests/Code/Models/UnitTest.cs
+++ RecellectionTests/Code/Models/UnitTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
+using Microsoft.Xna.Framework;
 
 namespace Recellection.Code.Models
 {
@@ -15,14 +16,38 @@ namespace Recellection.Code.Models
         public void init()
         {
             u1 = new Unit();
+            u2 = new Unit();
         }
 
         [Test]
-        public void createTest()
+        public void KillTest()
         {
             Assert.IsFalse(u1.IsDead());
             u1.Kill();
             Assert.IsTrue(u1.IsDead());
+        }
+
+        [Test]
+        public void MovementTest()
+        {
+            Assert.AreEqual(0, u2.GetPosition().X);
+            Assert.AreEqual(0, u2.GetPosition().Y);
+
+            Assert.AreEqual(-1, u2.GetTarget().X);
+            Assert.AreEqual(-1, u2.GetTarget().Y);
+
+            u2.SetTargetX(5,5);
+
+            for (int i = 0; i < 5; i++)
+            {
+                u2.Update(1);
+            }
+
+            Assert.AreEqual(5, u2.GetPosition().X);
+            Assert.AreEqual(5, u2.GetPosition().Y);
+
+            Assert.AreEqual(-1, u2.GetTarget().X);
+            Assert.AreEqual(-1, u2.GetTarget().Y);
         }
     }
 }
