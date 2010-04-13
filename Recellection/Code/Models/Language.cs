e0844b9e30092a01bb1f6df325035aae2452471e
squid-box <joelah@kth.se>
2010-04-13 18:46:59 +0200

    Fixed comments in Language.cs and added some constants in Unit.cs for readability.
    Signed-off-by: squid-box <joelah@kth.se>

--- Recellection/Code/Models/Language.cs
+++ Recellection/Code/Models/Language.cs
@@ -10,7 +10,7 @@ namespace Recellection.Code.Models
     /// </summary>
     /// <author>Joel Ahlgren</author>
     /// <date>2010-04-13</date>
-    public class Language : IModel
+    public static class Language : IModel
     {
 
     }
