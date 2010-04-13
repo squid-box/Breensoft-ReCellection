03aada408c24c0a2a2d3cef27d3bf46a70796caf
squid-box <joelah@kth.se>
2010-04-13 12:39:46 +0200

    Added my files required for Spring #2.
    Signed-off-by: squid-box <joelah@kth.se>

---
+++ Recellection/Code/Models/Language.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Recellection.Code.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <author>Joel Ahlgren</author>
+    /// <date>2010-04-13</date>
+    public class Language
+    {
+
+    }
+}
