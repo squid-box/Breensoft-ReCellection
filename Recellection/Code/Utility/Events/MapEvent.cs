da30f5bec67585b42185cfc132d2498e1cfd41ca
martin <martin.nycander@gmail.com>
2010-04-13 14:58:06 +0200

    Started working on World.cs
    Signed-off-by:  <>

---
+++ Recellection/Code/Utility/Events/MapEvent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using Recellection.Code.Models;
+
+namespace Recellection.Code.Utility.Events
+{
+    class MapEvent : Event<Tile[, ]>
+    {
+        public int row {get ; private set;}
+        public int col { get; private set;}
+
+        public MapEvent(Tile[,] map, int row, int col, EventType type)
+            : base(map, type)
+        {
+
+        }
+    }
+}
