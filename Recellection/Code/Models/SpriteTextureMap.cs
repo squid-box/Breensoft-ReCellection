f828df756dce3617e5f5bf2582fcdcbd77cd15cd
john <johnfor@kth.se>
2010-04-14 17:15:06 +0200

    I whould like to say that SpriteTextureMap is done. It is pure awsome!

--- Recellection/Code/Models/SpriteTextureMap.cs
+++ Recellection/Code/Models/SpriteTextureMap.cs
@@ -2,27 +2,61 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace Recellection.Code.Models
 {
+
+
     class SpriteTextureMap : IModel
     {
-        //Texture2D[] textures;
-        //GraphicsDevice[] graphicsDevices;
+        //Image file format, currently only accepts one format
+        public const String IMAGE_FORMAT = "png";
 
-        /*public void Init(GraphicsDevice[] graphicsDevices)
+        //The map which each of the texture image file will be placed
+        public const String TEXTURE_FOLDER = "Textures";
+
+        //The array containing each of the Texture2D.
+        Texture2D[] textures;
+
+        /// <summary>
+        /// Constructor for the SpriteTextureMap
+        /// </summary>
+        /// <param name="content">The ContentManager the
+        /// SpriteTextureMap will use to load the images</param>
+        public SpriteTextureMap(ContentManager content)
         {
-           this.graphicsDevices = graphicsDevices;
+            textures = new Texture2D[
+                Enum.GetValues(typeof(Globals.TextureTypes)).Length];
+
+            //Get all the names for the enum TextureTypes.
+
+            String[] textureNames = 
+                Enum.GetNames(typeof(Globals.TextureTypes));
+
+            for(int i = 0; i < textures.Length; i++) 
+            {
+                //For each of the enum name load the same texture image file.
+                textures[i] =
+                    content.Load<Texture2D>(
+
+                    TEXTURE_FOLDER+"/"+textureNames[i] + "." + IMAGE_FORMAT);
+
+            }
+
         }
-        */
-        /*
-        public Texture2D GetTexture(Globals.Texture texture, int curFrame, 
-            int size)
+        
+        /// <summary>
+        /// This method returns the Texture2D specified by the enum
+        /// Globals.TextureTypes.
+        /// </summary>
+        /// <param name="texture">The Globals.TextureTypes enum which 
+        /// specifies which Texture2D to return</param>
+        /// <returns>The requested Texture2D</returns>
+        public Texture2D GetTexture(Globals.TextureTypes texture)
         {
-            return new Texture2D(GraphicsDevice, size, size);
+            return textures[(int)texture];
         }
-        */
     }
 }
