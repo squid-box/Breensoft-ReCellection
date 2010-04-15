79215171cdef72c8148904eb4a008fa9363a9074
viktor <veklund@kth.se>
2010-04-15 23:03:39 +0200

    Dictionary my classes with more updated

--- Recellection/Code/Models/BarrierBuilding.cs
+++ Recellection/Code/Models/BarrierBuilding.cs
@@ -5,6 +5,12 @@ using System.Text;
 
 namespace Recellection.Code.Models
 {
+    /// <summary>
+    /// A BarrierBuilding improves upon an 
+    /// ordinary building by providing a defensive bonus
+    /// 
+    /// Author: Viktor Eklund
+    /// </summary>
     public class BarrierBuilding : Building
     {
         private readonly float powerBonus = 1.1f;
@@ -14,33 +20,20 @@ namespace Recellection.Code.Models
             get { return powerBonus; }
         }
         
+        /// <summary>
+        /// Constructs a new BarrierBuilding
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="posX"></param>
+        /// <param name="posY"></param>
+        /// <param name="maxHealth"></param>
+        /// <param name="owner"></param>
+        /// <param name="baseBuilding"></param>
         BarrierBuilding(String name, int posX, int posY, int maxHealth,
             Player owner, BaseBuilding baseBuilding)
             : base(name, posX, posY, maxHealth, owner, Globals.BuildingTypes.Barrier, baseBuilding)
         {
-            //unitsChanged += new global::Recellection.Code.Utility.Events.Publish<Building>(BarrierBuilding_unitsChanged);
+
         }
-
-        //void BarrierBuilding_unitsChanged(object publisher, global::Recellection.Code.Utility.Events.Event<Building> ev)
-        //{
-        //    if (ev.type == global::Recellection.Code.Utility.Events.EventType.ADD)
-        //    {
-
-        //    }
-        //    else if (ev.type == global::Recellection.Code.Utility.Events.EventType.REMOVE)
-        //    {
-
-        //    }
-        //}
-
-
-        //Not my job =(
-        //public void applyBonus()
-        //{
-        //    foreach (Unit unit in units)
-        //    {
-        //        unit.SetPowerLevel(powerBonus);
-        //    }
-        //}
     }
 }
