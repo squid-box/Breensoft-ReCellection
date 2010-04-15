11a1f09ad3ac59e7b710ce5073f0ad32d633e189
viktor <veklund@kth.se>
2010-04-15 17:46:14 +0200

    building tweaks

--- Recellection/Code/Models/BaseBuilding.cs
+++ Recellection/Code/Models/BaseBuilding.cs
@@ -5,38 +5,61 @@ using System.Text;
 
 namespace Recellection.Code.Models
 {
-    public class BaseBuilding : Building
-    {
-        LinkedList<Building> childBuildings;
+    public class BaseBuilding : ResourceBuilding // note that I inherit ResourceBuilding,
+    {                                            // this makes sense as a BaseBuilding 
+        LinkedList<Building> childBuildings;     // will have it's own production
 
         BaseBuilding(String name, int posX, int posY, int maxHealth,
-            Player owner, Globals.BuildingTypes type)
-            : base(name, posX, posY, maxHealth, owner, type, null)
+            Player owner)
+            : base(name, posX, posY, maxHealth, owner, null)
         {
-
+            this.type = Globals.BuildingTypes.Base;
         }
 
-        public void Visit(AggressiveBuilding building)
+        /// <summary>
+        /// allows a AggressiveBuilding to add itself to this basebuildings list of buildings
+        /// </summary>
+        /// <param name="building"></param>
+        public void Visit(Building building)
         {
-            
+            childBuildings.AddLast(building);    
         }
 
-        public void Visit(ResourceBuilding building)
-        {
-        }
+        ///// <summary>
+        ///// allows a ResourceBuilding to add itself to this basebuildings list of buildings
+        ///// </summary>
+        ///// <param name="building"></param>
+        //public void Visit(ResourceBuilding building)
+        //{
+        //    childBuildings.AddLast(building);
+        //}
 
-        public void Visit(BarrierBuilding building)
-        {
+        ///// <summary>
+        ///// allows a BarrierBuilding to add itself to this basebuildings list of buildings
+        ///// </summary>
+        ///// <param name="building"></param>
+        //public void Visit(BarrierBuilding building)
+        //{
+        //    childBuildings.AddLast(building);
+        //}
 
-        }
-
+        /// <summary>
+        /// Don't do it! it will break!
+        /// </summary>
+        /// <param name="building"></param>
         public void Visit(BaseBuilding building){
             throw new DivideByZeroException("");
         }
 
-        public void Visit(Building b)
+        /// <summary>
+        /// Gets an enumerator to this base buildings child buildings
+        /// </summary>
+        /// <returns>
+        /// The enumerator to the child buildings
+        /// </returns>
+        public LinkedList<Building>.Enumerator GetBuildings()
         {
+            return childBuildings.GetEnumerator();
         }
-
     }
 }
