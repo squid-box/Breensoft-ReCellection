40d4c3cb24120b99f0f169cd9642559b072f9e1c
unknown <Marco@.(none)>
2010-04-19 14:43:27 +0200

    Modified project file info. Did some shit.
    Signed-off-by:  <Mörkö>

--- Recellection/Code/Utility/Events/IPublisher.cs
+++ Recellection/Code/Utility/Events/IPublisher.cs
@@ -6,6 +6,5 @@ using Recellection.Code.Models;
 
 namespace Recellection.Code.Utility.Events
 {
-	public delegate void Publish<T, E>(Object publisher, E ev)
-		where E : Event<T> where T : IModel;
+	public delegate void Publish(IModel publisher);
 }
