b970d7611540f9d14203de7f4552b1de39e3db54
john <johnfor@kth.se>
2010-04-20 14:41:23 +0200

    BuildingController cant do anything...

---
+++ Recellection/Code/Controllers/BuildingController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Recellection.Code.Models;
+
+namespace Recellection.Code.Controllers
+{
+    class BuildingController
+    {
+        public BuildingController(Player player, World world)
+        {
+            foreach (Graph g in player.GetGraphs())
+            {
+                foreach (Building b in g.GetBuildings())
+                {
+
+                    if (b.type == Globals.BuildingTypes.Aggressive )
+                    {
+                        AcquireTarget(b);
+                    }
+                }
+            }
+
+        }
+
+        private void AcquireTarget(Building b)
+        {
+            
+        }
+
+    }
+}
