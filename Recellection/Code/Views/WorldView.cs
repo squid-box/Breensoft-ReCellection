a7b4c62b404342f2c68f8bf3291ead167768c872
Marco <marcoaj@kth.se>
2010-04-21 09:03:21 +0200

    Began working on WorldController.cs and WorldView.cs.
    Signed-off-by: Marco <marcoaj@kth.se>

--- Recellection/Code/Views/WorldView.cs
+++ Recellection/Code/Views/WorldView.cs
@@ -10,24 +10,66 @@ namespace Recellection.Code.Views
     /// <summary>
     /// The purpose of the World View is to provide the necessary data to render the game
     /// screen as described i the SRD 3.3. It also stores the information of the game state available
-    /// to the player.
+    /// to the player. The World View contains the information that is relevant to a single player, and
+    /// therefore has a reference to a Player-object.
     /// </summary>
     class WorldView
     {
-        private Player player;
+        /// <summary>
+        /// The player whose view of the world this is.
+        /// </summary>
+        public Player Player { get; private set; }
+        /// <summary>
+        /// The map of the world being viewed
+        /// </summary>
+        public Tile[,] Map { get; private set; }
+
+        // TODO Figure out the Rectangle class...
+        // public Rect CurrentScreen { get; set; }
+
         public WorldView(World world, Player player)
         {
-            this.player = player;
+            this.Player = player;
             world.MapEvent += OnMapEvent;
             world.TileEvent += OnTileEvent;
         }
 
+        /// <summary>
+        /// Defines the action to be taken when the MapEvent is invoked in World.
+        /// </summary>
+        /// <param name="o"></param>
+        /// <param name="ev"></param>
         public void OnMapEvent(Object o, Event<Tile[,]> ev)
+        {
+            Map = ev.subject; 
+        }
+
+        public void OnTileEvent(Object o, Event<Tile> ev)
+        {
+            
+        }
+
+        /// <summary>
+        /// I have no idea what this is supposed to do.
+        /// </summary>
+        /// <returns>Something</returns>
+        public Object GetDrawables()
         {
 
         }
 
-        public void OnTileEvent(Object o, Event<Tile> ev)
+        /// <summary>
+        /// I have no idea what this is supposed to do.
+        /// </summary>
+        public void UpdateScreen()
+        {
+
+        }
+
+        /// <summary>
+        /// I have no idea what this is supposed to do.
+        /// </summary>
+        public void UpdateMapMatrix()
         {
 
         }
