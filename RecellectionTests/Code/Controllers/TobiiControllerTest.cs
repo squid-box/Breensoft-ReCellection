1fd65960bb3e74020bf81e6e0cb050d3b0ccae92
viktor <veklund@kth.se>
2010-04-26 16:08:27 +0200

    test for tobii controller

---
+++ RecellectionTests/Code/Controllers/TobiiControllerTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Text;
+using NUnit.Framework;
+using Recellection.Code.Models;
+using Recellection.Code.Controllers;
+using Tobii.TecSDK.Client.Interaction.RegionImplementations;
+
+namespace Recellection
+{
+    [TestFixture]
+    class TobiiControllerTest
+    {
+        TobiiController tobiiController;
+        GUIRegion region1;
+        GUIRegion region2;
+        GUIRegion region3;
+        GUIRegion region4;
+        IntPtr dummyHandle;
+        Rect dummyRect;
+
+
+        [SetUp]
+        public void Init()
+        {
+            dummyRect = new Rect(0,0,0,0);
+            dummyHandle = new IntPtr(1);
+            tobiiController = TobiiController.GetInstance(dummyHandle);
+            tobiiController.Init();
+            region1 = new GUIRegion(dummyHandle, dummyRect);
+            region2 = new GUIRegion(dummyHandle, dummyRect);
+            region3 = new GUIRegion(dummyHandle, dummyRect);
+            region4 = new GUIRegion(dummyHandle, dummyRect);
+        }
+
+        [Test]
+        public void testIdentifiers1()
+        {
+            WindowBoundInteractionRegionIdentifier id1 = tobiiController.AddRegion(region1, Globals.RegionCategories.MainMenu);            
+            GUIRegion maybe_same1 = tobiiController.GetRegionByIdentifier(id1);
+            Assert.AreSame(maybe_same1, region1);            
+        }
+        [Test]
+        public void testRemove()
+        {
+            WindowBoundInteractionRegionIdentifier removeMe = tobiiController.AddRegion(region2, Globals.RegionCategories.MainMenu);
+            Assert.IsTrue(tobiiController.RemoveRegionByIdentifier(removeMe));
+            Assert.IsTrue(!tobiiController.RemoveRegionByIdentifier(removeMe));
+            //GUIRegion doesNotExist = tobiiController.GetRegionByIdentifier(removeMe);
+            //Assert.IsNull(doesNotExist);
+        }
+    }
+}
