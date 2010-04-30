70d654154fb159f510cc46f2d540f64ecaa12b73
mikolan <mmikkola@kth.se>
2010-04-30 13:10:55 +0200

    Changed 'Tobii Controller.cs' to 'TobiiController.cs' to conform to standards.

--- Recellection/Code/Controllers/Configurator.cs
+++ Recellection/Code/Controllers/Configurator.cs
@@ -5,7 +5,22 @@ using System.Text;
 
 namespace Recellection.Code.Controllers
 {
+    /// <summary>
+    /// Handles state management for the GameOptions Model.
+    /// 
+    /// Author: Mattias Mikkola
+    /// </summary>
     class Configurator
     {
+    }
+
+    private void BuildMenu()
+    {
+        /// TODO: write.
+    }; 
+
+    private void ChangeOptions()
+    {
+
     }
 }
