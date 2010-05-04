e4a50c76666f99f35c798ed3a8b1adcb599c1cb7
martin <martin.nycander@gmail.com>
2010-05-04 14:50:44 +0200

    Added a logic thread and sketched up how the program should run.

---
+++ Recellection/Code/Main/Initializer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Recellection.Code.Utility.Logger;
+
+namespace Recellection.Code.Main
+{
+	public class Initializer
+	{
+		private static Logger logger = LoggerFactory.GetLogger();
+
+		private GraphicsRenderer graphicRendering;
+		
+		public Initializer(GraphicsRenderer gfx)
+		{
+			logger.Debug("Initializer was instantiated.");
+			graphicRendering = gfx;
+		}
+		
+		public void Run()
+		{
+			logger.Debug("Initializer is running.");
+			
+			// TODO: Tell the graphic renderer what is the current view
+			
+			// TODO: Spawn main menu, tell it to run.
+		}
+	}
+}
