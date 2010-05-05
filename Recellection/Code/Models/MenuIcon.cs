f08e160c579898db96473a7a6298e3a8680b11d5
co <coer@kth.se>
2010-05-05 16:04:03 +0200

    more

---
+++ Recellection/Code/Models/MenuIcon.cs
@@ -0,0 +1,54 @@
+using System.Linq;
+using System.Text;
+using Tobii.TecSDK.Client.Interaction.RegionImplementations;
+using System;
+
+namespace Recellection
+{
+
+
+	public class MenuIcon
+	{
+		
+		private GUIRegion region;
+		private String label;
+		private Texture2D iconPic;
+		
+		public MenuIcon (String label, Texture2D iconPic)
+		{
+			region = null;
+			this.label = label;
+			this.iconPic = iconPic;
+		}
+		
+		public String getLabel()
+		{
+			return label;
+		}
+		
+		public Texture2D getIconPic()
+		{
+			return iconPic;
+		}
+		
+		public GUIregion getRegion()
+		{
+			return region;
+		}
+		
+		public void setRegion(GUIRegion region)
+		{
+			this.region = region;
+		}
+		
+//		public void setPosition(int x1, int y1, int x2, int y2)
+//		{
+//			
+//		}
+		
+//		public Rect getPosition()
+//		{
+//			return region.BoundingGeometry()
+//		}
+	}
+}
