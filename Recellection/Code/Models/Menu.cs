60fb26edb040a59e5c936208dab6e5a73b73908d
co <coer@kth.se>
2010-05-07 17:22:37 +0200

    And the fourth and final horseman is named Death

--- Recellection/Code/Models/Menu.cs
+++ Recellection/Code/Models/Menu.cs
@@ -15,6 +15,8 @@ namespace Recellection
 	{
 	    private List<MenuIcon> icons;
         private Texture2D menuPic;
+		private String text;
+		
 		
 		public Menu(Texture2D menuPic, List<MenuIcon> icons)
 		{
@@ -27,6 +29,11 @@ namespace Recellection
 			return icons;
 		}
 		
+		public Texture2D getMenuPic()
+		{
+			return menuPic;
+		}
+		
 		public List<GUIRegion> GetRegions()
 		{
 			List<GUIRegion> regions = new List<GUIRegion>();
@@ -37,8 +44,9 @@ namespace Recellection
 			return regions;
 		}
 		
-		public Menu(Globals.MenuLayout layout, List<MenuIcon> icons)
+		public Menu(Globals.MenuLayout layout, List<MenuIcon> icons, String text)
 		{
+			this.text = text;
 			switch(layout)
 			{
 				case Globals.MenuLayout.Prompt:
@@ -57,6 +65,9 @@ namespace Recellection
 			if (icons.Count != 2){
 				throw new Exception("Wrong amount of icons in menu");				
 			}
+			menuPic = Recellection.textureMap.GetTexture(Globals.TextureTypes(PromptMenu));
+			icons[0] = new MenuIcon("Yes", null);
+			icons[1] = new MenuIcon("No", null);
 			icons[0].setRegion(new GUIRegion(Recellection.windowHandle, new System.Windows.Rect(0, 0, Recellection.viewPort.Width * 2 / 5, Recellection.viewPort.Height)));
 			icons[1].setRegion(new GUIRegion(Recellection.windowHandle, new System.Windows.Rect(Recellection.viewPort.Width * 3 / 5, 0, Recellection.viewPort.Width, Recellection.viewPort.Height)));
 			this.icons = icons;
--- Recellection/Code/Views/MenuView.cs
+++ Recellection/Code/Views/MenuView.cs
@@ -1,18 +1,52 @@
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Recellection
 {
 
-
-	public class MenuView
+	public class MenuView : IDrawable
 	{
 		/// <summary>
 		/// author: co
 		/// </summary>
-
-		public MenuView ()
+	
+		private SpriteBatch textDrawer = new SpriteBatch(Recellection.graphics.GraphicsDevice);
+        private RenderTarget2D textRenderTex = new RenderTarget2D(Recellection.graphics.GraphicsDevice, Recellection.viewPort.Width, Recellection.viewPort.Height, 0, Recellection.graphics.GraphicsDevice.DisplayMode.Format);
+        private float fontSzInPx = 14;
+		List<DrawData> graphics;
+		
+		public MenuView()
 		{
+			MenuModel.Instance.MenuEvent += menuEventFunction;
+			graphics = new List<DrawData>();
+		}
+		
+		public void menuEventFunction(Object publisher, Event ev)
+		{
+			Menu m = ev.subject;
+			graphics.Clear();
+			graphics.Add(new DrawData(new Vector2D(0,0), m.getMenuPic(), 0, 0, Recellection.viewPort.Width));
+			//graphics.Add();//TODO skriv ut text SEN inte nu, laga menu
+			textDrawer.DrawString(Recellection.screenFont, Language.Instance.GetString("MainMenu1"), offset, Color.Black, 0, Vector2.Zero, textScale, SpriteEffects.None, 0);
+
+			if(ev.type == EventType.ADD)
+			{
+				Menu m = ev.subject;
+			}else if (ev.type == EventType.REMOVE)
+			{
+				
+			}
+		}
+		
+		
+		List<DrawData> GetDrawData()
+		{
+			return graphics;
 		}
 	}
 }
