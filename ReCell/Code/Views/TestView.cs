1821a334c2ddee1cb41eee0abaaeb45a6dbab3c0
squid-box <joelah@kth.se>
2010-05-07 17:43:31 +0200

    Remembered to add TestView.cs.

---
+++ ReCell/Code/Views/TestView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+namespace Recellection.Code.Views
+{
+    public class TestView : IRenderable
+    {
+        public List<DrawData> GetDrawData(ContentManager content)
+        {
+            Texture2D tex = content.Load<Texture2D>("Graphics/Terrains/art");
+            DrawData d = new DrawData(new Vector2(250, 250), tex, 0, 0, 128);
+            
+            List<DrawData> ret = new List<DrawData>();
+            ret.Add(d);
+
+            return ret;
+        }
+    }
+}
