cf72790b8d64ffd5432e3e391e3ab3e1bd1713bb
martin <martin.nycander@gmail.com>
2010-05-08 11:59:04 +0200

    GraphicRenderer - repaired!

--- ReCell/Code/Main/GraphicsRenderer.cs
+++ ReCell/Code/Main/GraphicsRenderer.cs
@@ -6,13 +6,14 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Recellection.Code.Views;
 using Microsoft.Xna.Framework.Content;
-
+using Recellection.Code.Utility.Logger;
 
 
 namespace Recellection
 {
     public class GraphicsRenderer
-    {
+	{
+		private static Logger logger = LoggerFactory.GetLogger();
 		public static IRenderable currentState = new TestView();
 		
         public GraphicsRenderer()
@@ -25,11 +26,20 @@ namespace Recellection
 			List<DrawData> drawData = GraphicsRenderer.currentState.GetDrawData(content);
             Recellection.graphics.GraphicsDevice.SetRenderTarget(0, null);
             Recellection.graphics.GraphicsDevice.Clear(Recellection.breen);
-
+			
+			//logger.Info("Viewport size: "+Recellection.viewPort.Width+"x"+Recellection.viewPort.Height);
+			
             spriteBatch.Begin(SpriteBlendMode.AlphaBlend, SpriteSortMode.Immediate, SaveStateMode.None);
             foreach (DrawData d in drawData)
             {
-                spriteBatch.Draw(d.Texture, d.Position, new Rectangle(d.CurrentFrame * d.SpriteHeight, 0, d.SpriteWidth, ((d.SpriteWidth+d.SpriteHeight)/2)), Color.White, d.Rotation, new Vector2(d.SpriteHeight / 2, d.SpriteWidth / 2), 1.0f, SpriteEffects.None, 0);
+                spriteBatch.Draw(d.Texture, 
+					new Rectangle((int)d.Position.X, (int)d.Position.Y, (int)d.Position.X+d.SpriteWidth, (int)d.Position.Y+d.SpriteHeight),
+					null,
+					Color.White, 
+					d.Rotation, 
+					new Vector2(0, 0),
+					SpriteEffects.None, 
+					0);
             }
             spriteBatch.End();
 
