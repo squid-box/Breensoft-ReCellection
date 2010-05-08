b543d13f1c3cb6b506c1fa4db119a66546568972
martin <martin.nycander@gmail.com>
2010-05-08 19:18:09 +0200

    Oh dear lord, it almost looks good! :D

--- ReCell/Code/Views/SplashView.cs
+++ ReCell/Code/Views/SplashView.cs
@@ -20,11 +20,6 @@ namespace Recellection.Code.Views
     public sealed class SplashView : IRenderable
     {
 
-        int x = 150;
-        int y = 150;
-        int width = 512;
-        int height = 150;
-
 		DrawData back;
 		DrawData front;
 		
@@ -43,7 +38,10 @@ namespace Recellection.Code.Views
 			Texture2D tex = Recellection.textureMap.GetTexture(Globals.TextureTypes.logo);
 
 			back = new DrawData(bg, new Rectangle(0, 0, Recellection.viewPort.Width, Recellection.viewPort.Height));
-			front = new DrawData(tex, new Rectangle(x, y, x + width, y + height), 0);
+
+			int x = Recellection.viewPort.Width / 2 - tex.Width / 2;
+			int y = Recellection.viewPort.Height / 2 - tex.Height / 2;
+			front = new DrawData(tex, new Rectangle(x, y, tex.Width, tex.Height), 0);
 
 			drawData = new List<DrawData>();
 			drawData.Add(back);
@@ -57,10 +55,7 @@ namespace Recellection.Code.Views
         /// <returns></returns>
         public List<DrawData> GetDrawData(ContentManager content)
         {
-			if (front.Opacity < 255)
-			{
-				front.Opacity += 5;
-			}
+			front.Opacity = (byte)Math.Min(front.Opacity + 5, 255);
 			
             return drawData;
         }
