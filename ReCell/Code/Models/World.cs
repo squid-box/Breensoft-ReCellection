4a8e968e120fdb6fb932235e05c4b08a461d2e64
john <johnfor@kth.se>
2010-05-16 15:02:30 +0200

    Started on fog of war... but then noo fail omg omg omg.

--- ReCell/Code/Models/World.cs
+++ ReCell/Code/Models/World.cs
@@ -213,5 +213,10 @@ namespace Recellection.Code.Models
         {
             return map;
         }
+
+        public bool isWithinMap(int x, int y)
+        {
+            return x > 0 && x < map.width && y > 0 && y < map.height;
+        }
     }
 }
