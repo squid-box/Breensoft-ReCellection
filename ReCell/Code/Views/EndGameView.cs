78447bb8768b7497654ececab1adf72cca369684
martin <martin.nycander@gmail.com>
2010-05-19 16:23:31 +0200

    And the lead said: Let there be translations! And there was. The lead saw this, and it was good.

--- ReCell/Code/Views/EndGameView.cs
+++ ReCell/Code/Views/EndGameView.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Recellection.Code.Models;
 namespace Recellection.Code.Views
 {
 	public class EndGameView : IView
@@ -25,15 +26,15 @@ namespace Recellection.Code.Views
 			
 			if (gameWasWon == true)
 			{
-				DrawCenteredString(sb, "Congratulations!\n\n... but I'm afraid the princess is in another castle.", new Vector2(Globals.VIEWPORT_WIDTH / 2, Globals.VIEWPORT_HEIGHT / 2), Color.White);
+				DrawCenteredString(sb, Language.Instance.GetString("hasWon"), new Vector2(Globals.VIEWPORT_WIDTH / 2, Globals.VIEWPORT_HEIGHT / 2), Color.White);
 			}
 			else if (gameWasWon == false)
 			{
-				DrawCenteredString(sb, "You lost.\n\nBetter luck next time!", new Vector2(Globals.VIEWPORT_WIDTH / 2, Globals.VIEWPORT_HEIGHT / 2), Color.White);
+				DrawCenteredString(sb, Language.Instance.GetString("hasLost"), new Vector2(Globals.VIEWPORT_WIDTH / 2, Globals.VIEWPORT_HEIGHT / 2), Color.White);
 			}
 			else
 			{
-				DrawCenteredString(sb, "The game has ended.\nDid you win or lose?\n\n... I do not know.", new Vector2(Globals.VIEWPORT_WIDTH / 2, Globals.VIEWPORT_HEIGHT / 2), Color.White);
+				DrawCenteredString(sb, Language.Instance.GetString("isDraw"), new Vector2(Globals.VIEWPORT_WIDTH / 2, Globals.VIEWPORT_HEIGHT / 2), Color.White);
 			}
 			
 
