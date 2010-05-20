352b4f09491c90fe57cb166f2b08d961362b692a
Marco <marcoaj@kth.se>
2010-05-20 17:43:33 +0200

    Forgot file
    Signed-off-by: Marco <marcoaj@kth.se>

---
+++ ReCell/Code/Utility/Pair.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Recellection.Code.Utility
+{
+	class Pair<T>
+	{
+		public T First {get; set; }
+		public T Second { get; set; }
+
+		public Pair(T first, T second)
+		{
+			this.First = first;
+			this.Second = second;
+		}
+	}
+}
