a763441dfcb8e45fd98debc85aa55e26f6fd98e5
mikolan <mmikkola@kth.se>
2010-05-20 21:46:19 +0200

    Units moving now clears some fog of war

--- ReCell/Code/Models/Unit.cs
+++ ReCell/Code/Models/Unit.cs
@@ -331,6 +331,17 @@ namespace Recellection.Code.Models
 			{
 				Unit.world.map.GetTile(beforeX, beforeY).RemoveUnit(this);
 				Unit.world.map.GetTile(afterX, afterY).AddUnit(this);
+                Unit.world.map.GetTile(afterX, afterY).MakeVisibleTo(this.owner);
+
+
+                // Let's update the fog of war!
+                for (int i = 1; i < 4; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        Unit.world.map.GetTile(afterX - j, afterY - i).MakeVisibleTo(this.owner);
+                    }
+                }
 			}
 		}
 
